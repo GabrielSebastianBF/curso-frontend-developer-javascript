89668f13c150926091bd39d91e96e84e8458f572
Lemut <gabriel.bustos@gmail.com>
2022-12-20 14:23:18 -0300

    cuarta fusion de codigo clase 21 - productos dinsmiacos desde js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -11,7 +12,7 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu(){
-    const isCarritoAsideClosed = aside.classList.contains('inactive')
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
     console.log('Click!');
     if (!isCarritoAsideClosed){
         aside.classList.add('inactive');
@@ -21,7 +22,7 @@ function toggleDesktopMenu(){
 
 
 function toggleMobileMenu(){
-    const isCarritoAsideClosed = aside.classList.contains('inactive')
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
     console.log('Click!');
     if (!isCarritoAsideClosed){
         aside.classList.add('inactive');
@@ -30,8 +31,8 @@ function toggleMobileMenu(){
 }
 
 function toggleCarritoAside(){
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
-    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
     console.log('Click!');
     
     if (!isMobileMenuClosed){
@@ -42,7 +43,80 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive');
 
         }
-   aside.classList.toggle('inactive')
+   aside.classList.toggle('inactive');
 }
 
+const productList = [];
+productList.push({
+    name:'Bike',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name:'Pantalla',
+    price: 240,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name:'Computador',
+    price: 500,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
+/* 
+<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+</div>
+ */
+
+function renderProducts(arr){
+
+for (product of arr){
+   const productCard = document.createElement('div');
+   productCard.classList.add('product-card');
+
+   const prodImg = document.createElement('img');
+   prodImg.setAttribute('src', product.image);
+
+   const productInfo = document.createElement('div');
+   productInfo.classList.add('product-info');
+
+   const productInfoDiv = document.createElement('div');
+
+   const productPrice = document.createElement('p');
+    productPrice.innerText = '$ ' + product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure')
+
+const productImgCart = document.createElement('img');
+productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+productInfoFigure.appendChild(productImgCart);
+
+productInfo.appendChild(productInfoDiv);
+productInfo.appendChild(productInfoFigure);
+
+productCard.appendChild(prodImg);
+productCard.appendChild(productInfo);
+
+cardsContainer.appendChild(productCard);
+
+
+}}
+
+renderProducts(productList);
